f2e97239d9d603a0e55e506e6c0d7260b41d0428
Reventlow <gorm@reventlow.com>
2022-10-30 17:11:57 +0100

    initial

--- django_azure_auth/settings.py
+++ django_azure_auth/settings.py
@@ -152,6 +152,9 @@ MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_PUBLIC_MEDIA_LOCATION)
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'),)
 
 # Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+# Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
