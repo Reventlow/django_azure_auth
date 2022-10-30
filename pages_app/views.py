03c945d06836d53d9dfb4e2c4b412ce6cae09ecd
Gorm Reventlow <gorm@reventlow.com>
2022-10-30 16:02:10 +0100

    Initial commit

---
+++ pages_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
+
+
+def home(request):
+    return render(request, 'home.html', {})
+
+
+
+def about(request):
+    return render(request, 'about.html', {})
