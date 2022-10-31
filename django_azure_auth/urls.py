aa00a631ceecffbdaf01a3b1a3aeee28dc33fee5
Reventlow <gorm@reventlow.com>
2022-10-31 13:14:40 +0100

    auth update

--- django_azure_auth/urls.py
+++ django_azure_auth/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
     path('microsoft/', include('microsoft_auth.urls', namespace='microsoft')),
     path('', include('pages_app.urls')),
 ]
